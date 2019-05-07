eb41965b7d5528525c25e705b4a5779d69a71e79
Palatino <p.alvarez.rio@gmail.com>
2019-05-07 21:29:03 +0100

    Add project files.

---
+++ Grasshopper_Exploration/Grasshopper_ExplorationInfo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Drawing;
+using Grasshopper.Kernel;
+
+namespace Grasshopper_Exploration
+{
+    public class Grasshopper_ExplorationInfo : GH_AssemblyInfo
+    {
+        public override string Name
+        {
+            get
+            {
+                return "GrasshopperExploration";
+            }
+        }
+        public override Bitmap Icon
+        {
+            get
+            {
+                //Return a 24x24 pixel bitmap to represent this GHA library.
+                return null;
+            }
+        }
+        public override string Description
+        {
+            get
+            {
+                //Return a short string describing the purpose of this GHA library.
+                return "";
+            }
+        }
+        public override Guid Id
+        {
+            get
+            {
+                return new Guid("e2372beb-91a0-48cb-a0d0-f58617e9e889");
+            }
+        }
+
+        public override string AuthorName
+        {
+            get
+            {
+                //Return a string identifying you or your company.
+                return "";
+            }
+        }
+        public override string AuthorContact
+        {
+            get
+            {
+                //Return a string representing your preferred contact details.
+                return "";
+            }
+        }
+    }
+}
